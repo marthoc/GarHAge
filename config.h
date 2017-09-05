70fdf6b22327e091ae68e57fcd1bc01852d5abe8
Mark Coombes <mark@markcoombes.ca>
2017-09-04 23:31:33 -0400

    Added comments to config.h

--- config.h
+++ config.h
@@ -6,18 +6,26 @@
  * User-configurable Parameters 
 */
 
+// Wifi Parameters
+
 #define WIFI_SSID "your-ssid-name"
 #define WIFI_PASSWORD "your-ssid-password"
+
+// Static IP Parameters
 
 #define STATIC_IP false
 #define IP 192,168,1,100
 #define GATEWAY 192,168,1,1
 #define SUBNET 255,255,255,0
 
+// MQTT Parameters
+
 #define MQTT_BROKER "w.x.y.z"
 #define MQTT_CLIENTID "GarHAge"
 #define MQTT_USERNAME "your-mqtt-username"
 #define MQTT_PASSWORD "your-mqtt-password"
+
+// Door 1 Parameters
 
 #define DOOR1_ALIAS "Door 1"
 #define MQTT_DOOR1_ACTION_TOPIC "garage/door/1/action"
@@ -26,6 +34,8 @@
 #define DOOR1_CLOSE_PIN D2
 #define DOOR1_STATUS_PIN D5
 #define DOOR1_STATUS_SWITCH_LOGIC "NO"
+
+// Door 2 Parameters
 
 #define DOOR2_ENABLED false
 #define DOOR2_ALIAS "Door 2"
